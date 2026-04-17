3b67c18b36caf2f440eb2acc3a9ffeda09e5cc2c
queenley <69098210+queenley@users.noreply.github.com>
2026-04-17 15:21:12 +0700

    Time: 4 ms (63.55%) | Memory: 10.7 MB (98.63%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int n = s.size();
+        if (numRows == 1 || numRows >= n) return s;        
+        string output(n, ' ');
+        int o_idx = 0;
+        for (int row = 0; row < numRows; row++) {
+            int s_idx = row;            
+            while (s_idx < n) {                                        
+                output[o_idx] = s[s_idx];
+                o_idx++;
+                if (s_idx % (numRows - 1) == row) {
+                    s_idx += (numRows - 1 - row) + (numRows - 1) - row; // numRows - 2 = number of elements on the diagonal
+                }
+                else s_idx += row * 2;
+                if (s_idx >= n) break;
+            }
+        }
+
+        return output;
+    }
+};

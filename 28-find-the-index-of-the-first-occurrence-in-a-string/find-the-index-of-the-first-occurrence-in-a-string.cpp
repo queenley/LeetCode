cfd1405ee7bc38116f386a2685771255904ebbde
queenley <69098210+queenley@users.noreply.github.com>
2026-04-19 15:50:08 +0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (46.35%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,10 +5,12 @@ public:
         int haystack_size = haystack.size();
         if (needle_size > haystack_size) return -1;
         int index = 0;        
-        while (index <= haystack_size - needle_size) {                        
+        while (index <= haystack_size - needle_size) {            
+            int i = index;
             for (int j = 0; j < needle_size; j++) {                
-                if (needle[j] == haystack[index + j]) {
-                    if (j == needle_size - 1) return index;                    
+                if (needle[j] == haystack[i]) {
+                    if (j == needle_size - 1) return index;
+                    i++;
                 }
                 else break;
             }

c772afbad3395d9d9efd31325cbbc133d3d3d59c
queenley <69098210+queenley@users.noreply.github.com>
2026-04-22 13:54:34 +0700

    Time: 4 ms (15.37%) | Memory: 10 MB (53.40%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,20 +3,13 @@ public:
     bool isPalindrome(string s) {        
         int start = 0, end = s.size() - 1;
         while (start < end) {            
-            if (!isalnum(s[start])) {
-                start++;
-                continue;
-            }            
-            if (!isalnum(s[end])) {
-                end--;
-                continue;
-            }
+            if (!isalnum(s[start])) { start++; continue; }
+            if (!isalnum(s[end])) { end--; continue; }
             if (tolower(s[start]) != tolower(s[end])) return false;
             start++;
-            end--;
-            if (start >= end) break;
+            end--;            
         }
-        
+
         return true;             
     }
 };

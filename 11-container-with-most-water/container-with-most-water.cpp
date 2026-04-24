e4fb38b0658522ff49acc1b5251b0d605471fb75
queenley <69098210+queenley@users.noreply.github.com>
2026-04-24 14:39:17 +0700

    Time: 0 ms (100.00%) | Memory: 63 MB (10.31%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,11 +3,12 @@ public:
     int maxArea(vector<int>& height) {
         int left_idx = 0, right_idx = height.size() - 1;
         int max_area = min(height[left_idx], height[right_idx]) * (right_idx - left_idx);
+        int _max_area = 0;
         while (left_idx < right_idx) {  
             if (height[left_idx] < height[right_idx]) left_idx++;
             else right_idx--;
 
-            int _max_area = min(height[left_idx], height[right_idx]) * (right_idx - left_idx);                          
+            _max_area = min(height[left_idx], height[right_idx]) * (right_idx - left_idx);                          
             if (_max_area > max_area) max_area = _max_area;            
         }
         return max_area;

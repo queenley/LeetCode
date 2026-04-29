0aa517991beced905a06461211289ad6dc04ef14
queenley <69098210+queenley@users.noreply.github.com>
2026-04-29 17:17:40 +0700

    Time: 4 ms (41.16%) | Memory: 12 MB (27.77%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,14 +1,13 @@
 #include <unordered_map>
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
-        if (ransomNote.size() > magazine.size()) return false;
+    bool canConstruct(string ransomNote, string magazine) {        
         unordered_map<char, int> magazine_dict;
         for (auto c: magazine) {            
             if (magazine_dict.find(c) == magazine_dict.end()) magazine_dict[c] = 1;            
             else magazine_dict[c]++;
         }
-        
+
         for (auto n: ransomNote) {
             if (magazine_dict[n] == 0 || magazine_dict.find(n) == magazine_dict.end()) return false;            
             magazine_dict[n]--;

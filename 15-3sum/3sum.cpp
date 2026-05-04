22c9948b9e744be23a448287a7ec2b8d88019509
queenley <69098210+queenley@users.noreply.github.com>
2026-05-04 10:52:12 +0700

    Time: 39 ms (95.34%) | Memory: 29 MB (71.72%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,21 +4,23 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> output;
         int n = nums.size();
-        for (int i = 0; i < n - 2; i++) {            
+        for (int i = 0; i < n - 2; i++) {     
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;            
             int left = i + 1, right = n - 1;
-            while (left < right) {
+            while (left < right) {                
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     output.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 }
                 else if (sum < 0) left++;
                 else right--;
             }            
-        }
-        sort(output.begin(), output.end());
-        output.erase(unique(output.begin(), output.end()), output.end());
+        }        
         return output;
     }
 };

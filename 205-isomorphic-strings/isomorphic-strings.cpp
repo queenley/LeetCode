f2a564c4234c4aa110953cc71fee8f61e2425a58
queenley <69098210+queenley@users.noreply.github.com>
2026-05-06 16:24:09 +0700

    Time: 2 ms (47.89%) | Memory: 9.4 MB (27.77%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> isomorphic;
+        unordered_map<char, char> isomorphic_reverse;
+        for (int i = 0; i < s.size(); i++) {
+            char sc = s[i], tc = t[i];
+            if (isomorphic.find(sc) != isomorphic.end() && isomorphic[sc] != tc) return false;
+            if (isomorphic_reverse.find(tc) != isomorphic_reverse.end() && isomorphic_reverse[tc] != sc) return false;      
+            
+            isomorphic[sc] = tc;
+            isomorphic_reverse[tc] = sc;
+        }
+        
+        return true;
+    }
+};

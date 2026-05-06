23d66f90ee4eb0fa646992ab6552c735467a10c8
queenley <69098210+queenley@users.noreply.github.com>
2026-05-06 16:37:18 +0700

    Time: 0 ms (100.00%) | Memory: 9.3 MB (55.01%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> isomorphic;
-        unordered_map<char, char> isomorphic_reverse;
+        // unordered_map<char, char> isomorphic;
+        // unordered_map<char, char> isomorphic_reverse;
         
+        // for (int i = 0; i < s.size(); i++) {
+        //     char sc = s[i], tc = t[i];
+        //     if (isomorphic.find(sc) != isomorphic.end() && isomorphic[sc] != tc) return false;
+        //     if (isomorphic_reverse.find(tc) != isomorphic_reverse.end() && isomorphic_reverse[tc] != sc) return false;      
+            
+        //     isomorphic[sc] = tc;
+        //     isomorphic_reverse[tc] = sc;
+        // }
+        
+        // return true;
+
+        int s_to_t[256];
+        int t_to_s[256];
+        memset(s_to_t, -1, sizeof(s_to_t));
+        memset(t_to_s, -1, sizeof(t_to_s));
+
         for (int i = 0; i < s.size(); i++) {
             char sc = s[i], tc = t[i];
-            if (isomorphic.find(sc) != isomorphic.end() && isomorphic[sc] != tc) return false;
-            if (isomorphic_reverse.find(tc) != isomorphic_reverse.end() && isomorphic_reverse[tc] != sc) return false;      
-            
-            isomorphic[sc] = tc;
-            isomorphic_reverse[tc] = sc;
+
+            if (s_to_t[sc] == -1 && t_to_s[tc] == -1) {
+                s_to_t[sc] = tc;
+                t_to_s[tc] = sc;
+            }
+            else if (s_to_t[sc] != tc || t_to_s[tc] != sc)
+                return false;
         }
-        
         return true;
     }
 };
